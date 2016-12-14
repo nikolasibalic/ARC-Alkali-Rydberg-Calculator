bf6f66250edcac8bddc3fb9ae4f7b4aad5e48e00
nikolasibalic <nikolasibalic@physics.org>
2016-12-14 17:14:38 +0000

    Update conf.py

--- doc_source/conf.py
+++ doc_source/conf.py
@@ -17,11 +17,11 @@ import os
 
 sys.path.insert(0,'../arc/')
 
-import mock
+#import mock
  
-MOCK_MODULES = ['numpy', 'scipy', 'matplotlib', 'matplotlib.pyplot', 'scipy.interpolate','scipy.constants','scipy.optimize','matplotlib.ticker']
-for mod_name in MOCK_MODULES:
-    sys.modules[mod_name] = mock.Mock()
+#MOCK_MODULES = ['numpy', 'scipy', 'matplotlib', 'matplotlib.pyplot', 'scipy.interpolate','scipy.constants','scipy.optimize','matplotlib.ticker']
+#for mod_name in MOCK_MODULES:
+#    sys.modules[mod_name] = mock.Mock()
 
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
