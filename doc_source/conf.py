f2a23f43eb762f5ca3313d94ca51b9e7aecc4946
nikolasibalic <nikolasibalic@physics.org>
2016-12-14 16:57:43 +0000

    Update conf.py

--- doc_source/conf.py
+++ doc_source/conf.py
@@ -21,7 +21,7 @@ import mock
  
 MOCK_MODULES = ['numpy', 'scipy', 'matplotlib', 'matplotlib.pyplot', 'scipy.interpolate','scipy.constants','scipy.optimize']
 for mod_name in MOCK_MODULES:
-sys.modules[mod_name] = mock.Mock()
+    sys.modules[mod_name] = mock.Mock()
 
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
