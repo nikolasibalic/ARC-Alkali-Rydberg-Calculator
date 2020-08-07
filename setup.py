24930decfa3e1f5dcda32ca08e2af8504a52e00a
Nikola Sibalic <nikolasibalic@physics.org>
2020-08-07 12:50:54 +0200

    version ++

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ arc_ext = Extension(
 
 setup(
     name="ARC-Alkali-Rydberg-Calculator",
-    version="v3.0.2",
+    version="v3.0.4",
     description="Alkali Rydberg Calculator",
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
@@ -29,7 +29,7 @@ setup(
               "quantum optics", "van der Waals", "scientific","atomic sensors",
               "quantum simulator","alkali atoms","alkaline atoms","divalent atoms"],
     url="https://github.com/nikolasibalic/ARC-Alkali-Rydberg-Calculator",
-    download_url="https://github.com/nikolasibalic/ARC-Alkali-Rydberg-Calculator/archive/v3.0.2.tar.gz",
+    download_url="https://github.com/nikolasibalic/ARC-Alkali-Rydberg-Calculator/archive/v3.0.4.tar.gz",
     author = 'Nikola Sibalic,  Elizabeth J. Robertson, Jonathan D. Pritchard, Robert M. Potvliege, Matthew P. A. Jones, Charles S. Adams, Kevin J. Weatherill',
     author_email = 'nikolasibalic@physics.org',
 
