21438d7fa1a45d2263f290a8b54dd8e3b92d2f94
Nikola Sibalic <nikolasibalic@physics.org>
2021-11-04 23:35:02 +0100

    reorganise

--- dev.py
+++
@@ -1,6 +0,0 @@
-# NOTE: Uncomment following two lines ONLY if you are not using installation via pip
-import sys, os
-sys.path.insert(0,".")  # add current directory in look up path for Python packages
-
-from arc import *
-from arc.beta import *  # This is necessary for running this notebook before official release of ARC 3.0.0
