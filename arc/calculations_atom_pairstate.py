de55bd5df61948bac539cae0351f4312a9482286
Matteo Pompili <matpompili@users.noreply.github.com>
2022-06-29 09:31:53 -0500

    Moved mpl style to optional file not imported by default

--- arc/calculations_atom_pairstate.py
+++ arc/calculations_atom_pairstate.py
@@ -56,19 +56,6 @@ import re
 import numpy as np
 from math import exp, sqrt
 import matplotlib.pyplot as plt
-import matplotlib as mpl
-mpl.rcParams['xtick.minor.visible'] = True
-mpl.rcParams['ytick.minor.visible'] = True
-mpl.rcParams['xtick.major.size'] = 8
-mpl.rcParams['ytick.major.size'] = 8
-mpl.rcParams['xtick.minor.size'] = 4
-mpl.rcParams['ytick.minor.size'] = 4
-mpl.rcParams['xtick.direction'] = 'in'
-mpl.rcParams['ytick.direction'] = 'in'
-mpl.rcParams['xtick.top'] = True
-mpl.rcParams['ytick.right'] = True
-mpl.rcParams['font.family'] = 'serif'
-
 
 # for matrices
 
---
+++ arc/mpl_style.py
@@ -0,0 +1,15 @@
+# This file contains some optional matplotlib style configurations.
+
+import matplotlib as mpl
+
+mpl.rcParams['xtick.minor.visible'] = True
+mpl.rcParams['ytick.minor.visible'] = True
+mpl.rcParams['xtick.major.size'] = 8
+mpl.rcParams['ytick.major.size'] = 8
+mpl.rcParams['xtick.minor.size'] = 4
+mpl.rcParams['ytick.minor.size'] = 4
+mpl.rcParams['xtick.direction'] = 'in'
+mpl.rcParams['ytick.direction'] = 'in'
+mpl.rcParams['xtick.top'] = True
+mpl.rcParams['ytick.right'] = True
+mpl.rcParams['font.family'] = 'serif'
