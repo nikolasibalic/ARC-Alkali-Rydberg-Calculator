Fixes #140 : uses round instead of int in case input paramters are affected by numerical precision of float variables

arc/alkali_atom_functions.py
@@ -823,7 +823,7 @@ class AlkaliAtom(object):
         defect = 0.0
         if l < 5:
             # find correct part in table of quantum defects
-            modifiedRRcoef = self.quantumDefect[int(floor(s) + s + j - l)][l]
+            modifiedRRcoef = self.quantumDefect[round(floor(s) + s + j - l)][l]
             if l < 3 and abs(modifiedRRcoef[0]) < 1e-9 and self.Z != 1:
                 # it's not Hydrogen but for l in {s,p,d} quantum defect is 0
                 raise ValueError(
@@ -846,7 +846,7 @@ class AlkaliAtom(object):
         return defect
 
     def getRadialMatrixElement(
-        self, n1, l1, j1, n2, l2, j2, s=0.5, useLiterature=True
+        self, n1:int, l1:int, j1:float, n2:int, l2:int, j2:float, s=0.5, useLiterature=True
     ):
         """
             Radial part of the dipole matrix element
@@ -891,12 +891,12 @@ class AlkaliAtom(object):
             j1 = j2
             j2 = temp
 
-        n1 = int(n1)
-        n2 = int(n2)
-        l1 = int(l1)
-        l2 = int(l2)
-        j1_x2 = int(round(2 * j1))
-        j2_x2 = int(round(2 * j2))
+        n1 = round(n1)
+        n2 = round(n2)
+        l1 = round(l1)
+        l2 = round(l2)
+        j1_x2 = round(2 * j1)
+        j2_x2 = round(2 * j2)
 
         c = self.conn.cursor()
 
@@ -966,7 +966,7 @@ class AlkaliAtom(object):
 
         return dipoleElement
 
-    def getQuadrupoleMatrixElement(self, n1, l1, j1, n2, l2, j2, s=0.5):
+    def getQuadrupoleMatrixElement(self, n1:int, l1:int, j1:float, n2:int, l2:int, j2:float, s=0.5):
         """
             Radial part of the quadrupole matrix element
 
@@ -1007,12 +1007,12 @@ class AlkaliAtom(object):
             j1 = j2
             j2 = temp
 
-        n1 = int(n1)
-        n2 = int(n2)
-        l1 = int(l1)
-        l2 = int(l2)
-        j1_x2 = int(round(2 * j1))
-        j2_x2 = int(round(2 * j2))
+        n1 = round(n1)
+        n2 = round(n2)
+        l1 = round(l1)
+        l2 = round(l2)
+        j1_x2 = round(2 * j1)
+        j2_x2 = round(2 * j2)
 
         c = self.conn.cursor()
         # was this calculated before? If yes, retrieve from memory.
@@ -1120,7 +1120,7 @@ class AlkaliAtom(object):
             j1 = j2
             j2 = temp
         return (
-            (-1) ** (int((l2 + l1 + 3.0) / 2.0 + s + j2))
+            (-1) ** (round((l2 + l1 + 3.0) / 2.0 + s + j2))
             * sqrt((2.0 * j2 + 1.0) * (2.0 * l1 + 1.0))
             * Wigner6j(l1, l2, 1, j2, j1, s)
             * sqrt(float(max(l1, l2)) / (2.0 * l1 + 1.0))
@@ -1173,7 +1173,7 @@ class AlkaliAtom(object):
         """
 
         return (
-            (-1) ** (int(l1 + s + j2 + 1.0))
+            (-1) ** (round(l1 + s + j2 + 1.0))
             * sqrt((2.0 * j1 + 1.0) * (2.0 * j2 + 1.0))
             * Wigner6j(j1, 1.0, j2, l2, s, l1)
             * self.getReducedMatrixElementL(n1, l1, j1, n2, l2, j2, s=s)
@@ -1863,8 +1863,8 @@ class AlkaliAtom(object):
             for row in data:
                 if count != 0:
                     # if not header
-                    n = int(row[0])
-                    l = int(row[1])
+                    n = round(row[0])
+                    l = round(row[1])
                     j = float(row[2])
                     A = float(row[3])
                     B = float(row[4])
@@ -1965,11 +1965,11 @@ class AlkaliAtom(object):
             i = 0
             for row in data:
                 if i != 0:
-                    n1 = int(row[0])
-                    l1 = int(row[1])
+                    n1 = round(row[0])
+                    l1 = round(row[1])
                     j1 = float(row[2])
-                    n2 = int(row[3])
-                    l2 = int(row[4])
+                    n2 = round(row[3])
+                    l2 = round(row[4])
                     j2 = float(row[5])
                     if self.getEnergy(n1, l1, j1) > self.getEnergy(n2, l2, j2):
                         temp = n1
@@ -1986,7 +1986,7 @@ class AlkaliAtom(object):
                     # to radial part of dme as it is saved for calculated
                     # values
                     dme = float(row[6]) / (
-                        (-1) ** (int(l1 + 0.5 + j2 + 1.0))
+                        (-1) ** (round(l1 + 0.5 + j2 + 1.0))
                         * sqrt((2.0 * j1 + 1.0) * (2.0 * j2 + 1.0))
                         * Wigner6j(j1, 1.0, j2, l2, 0.5, l1)
                         * (-1) ** l1
@@ -1995,7 +1995,7 @@ class AlkaliAtom(object):
                     )
 
                     comment = row[7]
-                    typeOfSource = int(row[8])  # 0 = experiment; 1 = theory
+                    typeOfSource = round(row[8])  # 0 = experiment; 1 = theory
                     errorEstimate = float(row[9])
                     ref = row[10]
                     refdoi = row[11]
@@ -2315,7 +2315,7 @@ class AlkaliAtom(object):
 
     def _reducedMatrixElementFJ(self, j1, f1, j2, f2):
         sph = 0.0
-        if (abs(f2 - f1) < 2) & (int(abs(j2 - j1)) < 2):
+        if (abs(f2 - f1) < 2) & (round(abs(j2 - j1)) < 2):
             # Reduced Matrix Element <f||er||f'> in units of reduced matrix element <j||er||j'>
             sph = (
                 (-1.0) ** (j1 + self.I + f2 + 1.0)
@@ -2750,7 +2750,7 @@ class AlkaliAtom(object):
         Pe = np.zeros(np.shape(Delta))
 
         # Loop over excited state energylevels
-        for fe in range(int(abs(je - self.I)), int(1.0 + (je + self.I))):
+        for fe in range(round(abs(je - self.I)), round(1.0 + (je + self.I))):
             # Hyperfine energy shift (rad s-1)
             Ehfs = 2.0 * np.pi * self.getHFSEnergyShift(je, fe, A, B)
             for mfe in range(
@@ -2914,7 +2914,7 @@ class AlkaliAtom(object):
         Pe = np.zeros(np.shape(Delta))
 
         # Loop over excited state energylevels
-        for fe in range(int(abs(je - self.I)), 1 + int(je + self.I)):
+        for fe in range(round(abs(je - self.I)), 1 + round(je + self.I)):
             # Hyperfine energy shift (rad s-1)
             Ehfs = 2.0 * np.pi * self.getHFSEnergyShift(je, fe, A, B)
             # range(max(-fe,min(mf1,mf0)-1),1+min(fe,max(mf1,mf0)+1)):
@@ -2993,11 +2993,11 @@ class AlkaliAtom(object):
         uB = physical_constants["Bohr magneton in Hz/T"][0]
 
         # Define Spin Matrices
-        N = int((2 * j + 1) * (2 * self.I + 1))
+        N = round((2 * j + 1) * (2 * self.I + 1))
         [jx, jy, jz] = self._spinMatrices(j)
-        ji = np.eye(int(2.0 * j + 1.0))
+        ji = np.eye(round(2.0 * j + 1.0))
         [ix, iy, iz] = self._spinMatrices(self.I)
-        ii = np.eye(int(2.0 * self.I + 1.0))
+        ii = np.eye(round(2.0 * self.I + 1.0))
 
         # Calculate Tensor Products
         Jx = np.kron(jx, ii)
@@ -3088,7 +3088,7 @@ def NumerovBack(innerLimit, outerLimit, kfun, step, init1, init2):
 
     """
 
-    br = int((sqrt(outerLimit) - sqrt(innerLimit)) / step)
+    br = round((sqrt(outerLimit) - sqrt(innerLimit)) / step)
     # integrated wavefunction R(r)*r^{3/4}
     sol = np.zeros(br, dtype=np.dtype("d"))
     # radial coordinate for integration \sqrt(r)
@@ -3391,8 +3391,8 @@ def loadSavedCalculation(fileName:str):
 
 
 def singleAtomState(j, m):
-    a = np.zeros((int(round(2.0 * j + 1.0, 0)), 1), dtype=np.complex128)
-    a[int(round(j + m, 0))] = 1
+    a = np.zeros((round(2.0 * j + 1.0), 1), dtype=np.complex128)
+    a[round(j + m)] = 1
     return a
 
 

arc/calculations_atom_pairstate.py
@@ -442,8 +442,8 @@ class PairStateInteractions:
 
         am = np.zeros(
             (
-                int(round((2 * j1 + 1) * (2 * j2 + 1), 0)),
-                int(round((2 * j + 1) * (2 * jj + 1), 0)),
+                round((2 * j1 + 1) * (2 * j2 + 1)),
+                round((2 * j + 1) * (2 * jj + 1)),
             ),
             dtype=np.float64,
         )
@@ -453,31 +453,25 @@ class PairStateInteractions:
 
         j1range = np.linspace(-j1, j1, round(2 * j1) + 1)
         j2range = np.linspace(-j2, j2, round(2 * j2) + 1)
-        jrange = np.linspace(-j, j, int(2 * j) + 1)
-        jjrange = np.linspace(-jj, jj, int(2 * jj) + 1)
+        jrange = np.linspace(-j, j, round(2 * j) + 1)
+        jjrange = np.linspace(-jj, jj, round(2 * jj) + 1)
 
         for m1 in j1range:
             for m2 in j2range:
                 # we have chosen the first index
-                index1 = int(
-                    round(
+                index1 = round(
                         m1 * (2.0 * j2 + 1.0)
                         + m2
-                        + (j1 * (2.0 * j2 + 1.0) + j2),
-                        0,
+                        + (j1 * (2.0 * j2 + 1.0) + j2)
                     )
-                )
                 for m in jrange:
                     for mm in jjrange:
                         # we have chosen the second index
-                        index2 = int(
-                            round(
+                        index2 = round(
                                 m * (2.0 * jj + 1.0)
                                 + mm
-                                + (j * (2.0 * jj + 1.0) + jj),
-                                0,
+                                + (j * (2.0 * jj + 1.0) + jj)
                             )
-                        )
 
                         # angular matrix element from Sa??mannshausen, Heiner,
                         # Merkt, Fr??d??ric, Deiglmayr, Johannes
@@ -1261,8 +1255,8 @@ class PairStateInteractions:
 
         interactionMatrix = np.zeros(
             (
-                int(round((2 * self.j + 1) * (2 * self.jj + 1))),
-                int(round((2 * self.j + 1) * (2 * self.jj + 1))),
+                round((2 * self.j + 1) * (2 * self.jj + 1)),
+                round((2 * self.j + 1) * (2 * self.jj + 1)),
             ),
             dtype=np.complex,
         )
@@ -1855,8 +1849,8 @@ class PairStateInteractions:
                 print("Finding coupling strengths")
             # get first what was the state we are calculating coupling with
             state1 = drivingFromState
-            n1 = int(round(state1[0]))
-            l1 = int(round(state1[1]))
+            n1 = round(state1[0])
+            l1 = round(state1[1])
             j1 = state1[2]
             m1 = state1[3]
             q = state1[4]
@@ -1865,7 +1859,7 @@ class PairStateInteractions:
                 thisCoupling = 0.0
 
                 if (
-                    int(abs(self.basisStates[i][5] - l1)) == 1
+                    round(abs(self.basisStates[i][5] - l1)) == 1
                     and abs(
                         self.basisStates[i][0]
                         - self.basisStates[self.originalPairStateIndex][0]
@@ -1888,8 +1882,8 @@ class PairStateInteractions:
                     < 0.1
                 ):
                     state2 = self.basisStates[i]
-                    n2 = int(state2[0 + 4])
-                    l2 = int(state2[1 + 4])
+                    n2 = round(state2[0 + 4])
+                    l2 = round(state2[1 + 4])
                     j2 = state2[2 + 4]
                     m2 = state2[3 + 4]
                     if debugOutput:
@@ -2069,10 +2063,10 @@ class PairStateInteractions:
             % (
                 self.atom1.elementName,
                 printStateString(self.n, self.l, self.j),
-                int(round(2.0 * self.m1)),
+                round(2.0 * self.m1),
                 self.atom2.elementName,
                 printStateString(self.nn, self.ll, self.jj),
-                int(round(2.0 * self.m2)),
+                round(2.0 * self.m2),
             )
         )
         if self.interactionsUpTo == 1:
@@ -2137,7 +2131,7 @@ class PairStateInteractions:
                     + "   %s m_j =%d/2 with polarization q=%d.\n"
                     % (
                         printStateString(*self.drivingFromState[0:3]),
-                        int(round(2.0 * self.drivingFromState[3])),
+                        round(2.0 * self.drivingFromState[3]),
                         self.drivingFromState[4],
                     )
                 )
@@ -2252,26 +2246,26 @@ class PairStateInteractions:
             # Alkali atom
             stateString += "|%s %d/2" % (
                 printStateStringLatex(n1, l1, j1, s=self.s1),
-                int(2 * mj1),
+                round(2 * mj1),
             )
         else:
             # divalent atoms
             stateString += "|%s %d" % (
                 printStateStringLatex(n1, l1, j1, s=self.s1),
-                int(mj1),
+                round(mj1),
             )
 
         if abs(self.s2 - 0.5) < 0.1:
             # Alkali atom
             stateString += ",%s %d/2\\rangle" % (
                 printStateStringLatex(n2, l2, j2, s=self.s2),
-                int(2 * mj2),
+                round(2 * mj2),
             )
         else:
             # divalent atom
             stateString += ",%s %d\\rangle" % (
                 printStateStringLatex(n2, l2, j2, s=self.s2),
-                int(mj2),
+                round(mj2),
             )
         return stateString
 
@@ -2374,26 +2368,26 @@ class PairStateInteractions:
                 # Alkali atom
                 label += r"$|\langle %s m_j=%d/2 " % (
                     printStateStringLatex(self.n, self.l, self.j),
-                    int(round(2.0 * self.m1, 0)),
+                    round(2.0 * self.m1),
                 )
             else:
                 # divalent atom
                 label += r"$|\langle %s m_j=%d " % (
                     printStateStringLatex(self.n, self.l, self.j, s=self.s1),
-                    int(round(self.m1, 0)),
+                    round(self.m1),
                 )
 
             if abs(self.s2 - 0.5) < 0.1:
                 # Alkali atom
                 label += r", %s m_j=%d/2 | \mu \rangle |^2$" % (
                     printStateStringLatex(self.nn, self.ll, self.jj),
-                    int(round(2.0 * self.m2, 0)),
+                    round(2.0 * self.m2),
                 )
             else:
                 # divalent atom
                 label += r", %s m_j=%d | \mu \rangle |^2$" % (
                     printStateStringLatex(self.nn, self.ll, self.jj, s=self.s2),
-                    int(round(self.m2, 0)),
+                    round(self.m2, 0),
                 )
 
             cb.set_label(label)

arc/calculations_atom_single.py
@@ -894,8 +894,8 @@ class StarkMap:
                 print("Finding driving field coupling...")
             # get first what was the state we are calculating coupling with
             state1 = drivingFromState
-            n1 = int(round(state1[0]))
-            l1 = int(round(state1[1]))
+            n1 = round(state1[0])
+            l1 = round(state1[1])
             j1 = state1[2]
             m1 = state1[3]
             q = state1[4]
@@ -908,13 +908,13 @@ class StarkMap:
                     )
                     sys.stdout.flush()
                 if (
-                    (int(abs(self.basisStates[i][1] - l1)) == 1)
-                    and (int(abs(self.basisStates[i][2] - j1)) <= 1)
-                    and (int(abs(self.basisStates[i][3] - m1 - q)) == 0)
+                    (round(abs(self.basisStates[i][1] - l1)) == 1)
+                    and (round(abs(self.basisStates[i][2] - j1)) <= 1)
+                    and (round(abs(self.basisStates[i][3] - m1 - q)) == 0)
                 ):
                     state2 = self.basisStates[i]
-                    n2 = int(state2[0])
-                    l2 = int(state2[1])
+                    n2 = round(state2[0])
+                    l2 = round(state2[1])
                     j2 = state2[2]
                     m2 = state2[3]
                     if debugOutput:
@@ -1052,7 +1052,7 @@ class StarkMap:
         commonHeader += "\n *** Stark Map for %s %s m_j = %d/2. ***\n\n" % (
             self.atom.elementName,
             printStateString(self.n, self.l, self.j),
-            int(round(2.0 * self.mj)),
+            round(2.0 * self.mj),
         )
         commonHeader += (
             " - Included states - principal quantum number (n) range [%d-%d].\n"
@@ -1078,7 +1078,7 @@ class StarkMap:
                 + "   %s m_j =%d/2 with polarization q=%d.\n"
                 % (
                     printStateString(*self.drivingFromState[0:3]),
-                    int(round(2.0 * self.drivingFromState[3])),
+                    round(2.0 * self.drivingFromState[3]),
                     self.drivingFromState[4],
                 )
             )
@@ -1421,13 +1421,13 @@ class StarkMap:
             # we have Alkali Atoms
             return "|%s m_j=%d/2\\rangle" % (
                 printStateStringLatex(n1, l1, j1),
-                int(2 * mj1),
+                round(2 * mj1),
             )
         else:
             # we have singlets or triplets states of divalent atoms
             return "|%s m_j=%d\\rangle" % (
                 printStateStringLatex(n1, l1, j1, s=self.s),
-                int(mj1),
+                round(mj1),
             )
 
     def getPolarizability(
@@ -2263,10 +2263,10 @@ class AtomSurfaceVdW:
             )
 
         # sum over mj1
-        for mj1 in np.linspace(-j1, j1, int(round(2 * j1 + 1))):
+        for mj1 in np.linspace(-j1, j1, round(2 * j1 + 1)):
             # calculate sum_mj2 |<j1,mj1|Dx|j2,mj2>|^2 + |<j1,mj1|Dy|j2,mj2>|^2 + 2* |<j1,mj1|Dz|j2,mj2>|^2
             # which is equal to (check!)  |<j1,mj1|D+|j2,mj2>|^2 + |<j1,mj1|D-|j2,mj2>|^2 + 2* |<j1,mj1|Dz|j2,mj2>|^2
-            for mj2 in np.linspace(-j2, j2, int(round(2 * j2 + 1, 0))):
+            for mj2 in np.linspace(-j2, j2, round(2 * j2 + 1)):
                 for q in [-1, +1]:
                     result += (
                         abs(

arc/wigner.py
@@ -75,11 +75,11 @@ def Wigner3j(j1, j2, j3, m1, m2, m3):
         ):
             # return precalculated value
             return wignerPrecal3j[
-                int(roundPy2(2 * j1)),
-                int(roundPy2(2 * (wignerPrecalJmax + m1))),
-                int(roundPy2(2.0 * j2)),
-                int(roundPy2(m2 + j2)),
-                int(roundPy2(2 - j3 + j1)),
+                round(roundPy2(2 * j1)),
+                round(roundPy2(2 * (wignerPrecalJmax + m1))),
+                round(roundPy2(2.0 * j2)),
+                round(roundPy2(m2 + j2)),
+                round(roundPy2(2 - j3 + j1)),
             ]
         else:
             # that value is 0
@@ -265,10 +265,10 @@ def Wigner6j(j1, j2, j3, J1, J2, J3):
         return wignerPrecal6j[
             j1,
             2 + j1 - J3,
-            int(roundPy2(2 + 2 * (j3 - j1))),
-            int(roundPy2(2 + 2 * (J1 - J3))),
+            round(roundPy2(2 + 2 * (j3 - j1))),
+            round(roundPy2(2 + 2 * (J1 - J3))),
             J2 - 1,
-            int(roundPy2(2 * j2)),
+            round(roundPy2(2 * j2)),
         ]
     # print("not in database %1.f %1.f %1.f %1.f %1.f %1.f" % (j1,j2,j3,J1,J2,J3))
 
@@ -509,22 +509,22 @@ class WignerDmatrix:
         """
         if self.trivial:
             return sparse_eye(
-                int(roundPy2(2.0 * j + 1.0)),
-                int(roundPy2(2.0 * j + 1.0)),
+                round(roundPy2(2.0 * j + 1.0)),
+                round(roundPy2(2.0 * j + 1.0)),
                 dtype=np.complex128,
             )
-        savedIndex = self.matLoc[int(roundPy2(2 * j))]
+        savedIndex = self.matLoc[round(roundPy2(2 * j))]
         if savedIndex != 0:
             return self.matSaved[savedIndex - 1]
             # bacause 0 marks no entry; but matrix numbers starts from zero,
             # saved Index array is actually offsetted by 1
         # else
         mat = np.zeros(
-            (int(roundPy2(2.0 * j + 1.0)), int(roundPy2(2.0 * j + 1.0))),
+            (round(roundPy2(2.0 * j + 1.0)), round(roundPy2(2.0 * j + 1.0))),
             dtype=np.complex128,
         )
-        jrange = np.linspace(-j, j, int(2 * j) + 1)
-        maxIndex = int(2 * j) + 1
+        jrange = np.linspace(-j, j, round(2 * j) + 1)
+        maxIndex = round(2 * j) + 1
 
         for index1 in xrange(maxIndex):
             for index2 in xrange(maxIndex):
@@ -534,5 +534,5 @@ class WignerDmatrix:
 
         mat = csr_matrix(mat)
         self.matSaved.append(mat)
-        self.matLoc[int(roundPy2(2 * j))] = len(self.matSaved)
+        self.matLoc[round(roundPy2(2 * j))] = len(self.matSaved)
         return mat