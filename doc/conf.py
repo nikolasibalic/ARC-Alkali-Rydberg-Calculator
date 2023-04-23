942c8f8fccb50e92d79af69d6dd9017811f8d139
Nikola Sibalic <nikolasibalic@physics.org>
2023-04-23 16:27:25 +0200

    final format

--- doc/conf.py
+++ doc/conf.py
@@ -16,7 +16,6 @@ import sys
 import os
 
 
-
 sys.path.insert(0, os.path.abspath(".."))
 # resolve problem with not having _tkinter on server used for documentation building
 autodoc_mock_imports = ["_tkinter"]
