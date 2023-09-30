nikolasibalic/ARC-Alkali-Rydberg-Calculator

commit 8b047d93582ecb186b858f8761dc8ceed646ef05

Fix: change deprecated types in Numpy 1.20.0, fixes #146

arc/calculations_atom_pairstate.py
@@ -580,7 +580,7 @@ class PairStateInteractions:
         data[:, 5] *= 2  # j3 -> 2 r j3
         data[:, 7] *= 2  # j4 -> 2 r j4
 
-        data = np.array(np.rint(data), dtype=np.int)
+        data = np.array(np.rint(data), dtype=np.int16)
 
         try:
             c = self.conn.cursor()
@@ -1260,7 +1260,7 @@ class PairStateInteractions:
                 round((2 * self.j + 1) * (2 * self.jj + 1)),
                 round((2 * self.j + 1) * (2 * self.jj + 1)),
             ),
-            dtype=np.complex,
+            dtype=np.complex128,
         )
 
         for n1 in xrange(max(self.n - nRange, 1), self.n + nRange + 1):

commit e0e237c4b828c74ebf5d7c65254b18dd4fcf8870

fix: regression introduced in 8b047d93582ecb186b858f8761dc8ceed646ef05 - use int instead of np.int16

arc/calculations_atom_pairstate.py
@@ -580,7 +580,7 @@ class PairStateInteractions:
         data[:, 5] *= 2  # j3 -> 2 r j3
         data[:, 7] *= 2  # j4 -> 2 r j4
 
-        data = np.array(np.rint(data), dtype=np.int16)
+        data = np.array(np.rint(data), dtype=int)
 
         try:
             c = self.conn.cursor()