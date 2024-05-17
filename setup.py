e92da263e738cb94f9cfb2d73419840b63c4b020
David Meyer <dihm.meyer@gmail.com>
2024-05-17 12:52:49 -0400

    Fix minor issue build where -O3 is passed to MSVC on windows, which it doesn't recognize.
    Also remove old distutils code as no longer supported for python>=3.8

--- setup.py
+++ setup.py
@@ -2,11 +2,8 @@
 # -*- mode: Python; coding: utf-8 -*-
 
 
-try:
-    from setuptools import setup, Extension
-except ImportError:
-    from distutils.core import setup
-    from distutils.extension import Extension
+from setuptools import setup, Extension
+import platform
 
 
 class get_numpy_include_dirs(object):
@@ -19,10 +16,15 @@ class get_numpy_include_dirs(object):
         return np.get_include()
 
 
+if platform.system() == "Windows":
+    c_args = ["/Wall", "/O2"]
+else:
+    c_args = ["-Wall", "-O3"]
+
 arc_ext = Extension(
     "arc.arc_c_extensions",
     sources=["arc/arc_c_extensions.c"],
-    extra_compile_args=["-Wall", "-O3"],
+    extra_compile_args=c_args,
     include_dirs=[get_numpy_include_dirs()],
 )
 
