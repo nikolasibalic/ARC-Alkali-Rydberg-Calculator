480bcac34facdda0ccaf8b6a14cff9c2c6d7843d
David Meyer <dihm.meyer@gmail.com>
2024-05-17 12:48:21 -0400

    Create pyproject.toml
    Moves all meta-data to pyproject.toml.
    Moves to a PEP-517 compliant build backend specification.
    Moves all build and install dependency specification to pyproject.toml.

--- setup.py
+++ setup.py
@@ -28,53 +28,5 @@ arc_ext = Extension(
 
 
 setup(
-    name="ARC-Alkali-Rydberg-Calculator",
-    version="3.4.1",
-    description="Alkali Rydberg Calculator",
-    long_description=open("README.md").read(),
-    long_description_content_type="text/markdown",
-    license="BSD3",
-    keywords=[
-        "rydberg",
-        "physics",
-        "stark maps",
-        "atom interactions",
-        "quantum optics",
-        "van der Waals",
-        "scientific",
-        "atomic sensors",
-        "quantum simulator",
-        "alkali atoms",
-        "alkaline atoms",
-        "divalent atoms",
-        "quantum computing",
-    ],
-    classifiers=[
-        "Programming Language :: Python",
-        "Programming Language :: Python :: 3",
-        "Intended Audience :: Science/Research",
-        "Intended Audience :: Education",
-        "License :: OSI Approved :: BSD License",
-        "Topic :: Scientific/Engineering",
-        "Topic :: Scientific/Engineering :: Physics",
-        "Development Status :: 5 - Production/Stable",
-    ],
-    setup_requires=["oldest-supported-numpy"],
-    url="https://github.com/nikolasibalic/ARC-Alkali-Rydberg-Calculator",
-    download_url="https://github.com/nikolasibalic/ARC-Alkali-Rydberg-Calculator/archive/refs/tags/v3.4.1.tar.gz",
-    author="Nikola Sibalic,  Elizabeth J. Robertson, Jonathan D. Pritchard, Robert M. Potvliege, Matthew P. A. Jones, Charles S. Adams, Kevin J. Weatherill, and contributors",
-    author_email="nikolasibalic@physics.org",
-    packages=["arc", "arc.advanced"],
-    package_data={
-        "arc": ["data/*", "data/refractive_index_data/*", "arc_c_extensions.c"]
-    },
-    install_requires=[
-        "scipy>=0.18.1",
-        "numpy>=1.16.0",
-        "matplotlib>=1.5.3",
-        "sympy>=1.1.1",
-        "lmfit>=0.9.0",
-    ],
-    zip_safe=False,
     ext_modules=[arc_ext],
 )
